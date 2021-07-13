e19e3315ebdb287d91fe13dcdd9f9defe1e9c693
Muhammad Rana <muhammadrana523@gmail.com>
2021-07-13 16:59:59 +0500

    Changes

--- resources/views/invoice/index.blade.php
+++ resources/views/invoice/index.blade.php
@@ -5,8 +5,8 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="py-2">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-1">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <form class="mb-4" method="GET" action="{{ route('invoice.index') }}">
